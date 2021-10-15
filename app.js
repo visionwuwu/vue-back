4934c6e66df4654e2487a81621091bb2dc0b2c14
baowu <2021664244@qq.com>
2021-10-15 22:19:14 +0800

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const { createServer } = require('http');
+
+const users = [
+  { id: 1, name: 'Visionwuwu', age: 18, sex: '男', cart: '劳斯莱斯' },
+  { id: 2, name: 'yan', age: 18, sex: '女' },
+];
+
+const server = createServer((req, res) => {
+  const { url, method } = req;
+  if (method.toUpperCase() === 'GET' && url === '/api/users') {
+    const result = JSON.stringify({
+      code: 200,
+      data: users
+    });
+    res.end(result);
+  } else {
+    res.end('Not Found！！！');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('server is running 3000 port');
+});
